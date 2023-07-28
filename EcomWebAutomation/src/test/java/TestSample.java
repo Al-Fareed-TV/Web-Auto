9e9e5f641970f3e15b7472127d57a0a52b261301
Al-Fareed-TV <140706199+Al-Fareed-TV@users.noreply.github.com>
2023-07-28 11:57:15 +0530

    initial commit

---
+++ EcomWebAutomation/src/test/java/TestSample.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSample {
+    @Test
+    public void testMain(){
+        System.out.println("Hello");
+    }
+
+
+}
