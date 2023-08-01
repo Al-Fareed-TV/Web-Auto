67504291862beb10db4a73b87e06f1d15bf04483
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-08-01 11:19:43 +0530

    added skeleton of Home,Item and Launcher Page

---
+++ EcommWebAutomation/src/main/java/org/example/HomePage.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class HomePage {
+    public HomePage(WebDriver webDriver) {
+    }
+    public void search(String searchItem) {
+    }
+    public List<Item> getSearchItems() {
+        return new ArrayList<>();
+    }
+}
