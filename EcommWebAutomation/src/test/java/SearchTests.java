2319c0e923b35ecae92f842d7275fc2bda453d6f
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-08-09 12:47:56 +0530

    Update Search Test Pages with locators Using WebDriver API

--- EcommWebAutomation/src/test/java/SearchTests.java
+++ EcommWebAutomation/src/test/java/SearchTests.java
@@ -2,6 +2,7 @@ import org.example.HomePage;
 import org.example.Item;
 import org.example.LauncherPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,22 +11,82 @@ import java.util.List;
 public class SearchTests {
     @Test
     public void verifyIfSearchTermShowsRelevantResults() {
-        //Arrange
-        String searchItem = "Product";
-        String searchKey = "Product";
-        WebDriver webDriver = null; //Dummy WebDriver declaration
+
+        WebDriver webDriver = new ChromeDriver();
+        try { //Arrange
+            String searchItem = "Product";
+            String searchKey = "Product";
+            LauncherPage launcherPage = new LauncherPage(webDriver);
+            launcherPage.navigateTo("https://web-playground.ultralesson.com/");
+
+            //Act
+            HomePage homepage = new HomePage(webDriver);
+            homepage.search(searchItem);
+            List<Item> searchItems = homepage.getSearchItems();
+
+            //Assert
+            Assert.assertEquals(4, searchItems.size());
+            Assert.assertTrue(searchItems.stream().allMatch(item -> item.getName().contains(searchKey)));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            webDriver.quit();
+        }
+    }
+
+    @Test
+    public void verifySearchUnavailableProduct() {
+        // Arrange
+        String unavailableProduct = "Unobtainium Widget";
+        WebDriver webDriver = null;
+
         LauncherPage launcherPage = new LauncherPage(webDriver);
         launcherPage.navigateTo("https://web-playground.ultralesson.com/");
 
-        //Act
+        // Act
+        HomePage homepage = new HomePage(webDriver);
+        homepage.search(unavailableProduct);
+        List<Item> searchItems = homepage.getSearchItems();
+
+        // Assert
+        Assert.assertTrue(searchItems.isEmpty());
+    }
+
+    @Test
+    public void verifyBrandSearchListsOnlyBrandItems() {
+        // Arrange
+        String brandName = "Nike";
+        WebDriver webDriver = null;
+
+        LauncherPage launcherPage = new LauncherPage(webDriver);
+        launcherPage.navigateTo("https://web-playground.ultralesson.com/");
+
+        // Act
+        HomePage homepage = new HomePage(webDriver);
+        homepage.search(brandName);
+        List<Item> searchItems = homepage.getSearchItems();
+
+        // Assert
+        Assert.assertTrue(searchItems.stream().allMatch(item -> item.getName().contains(brandName)));
+    }
+
+    @Test
+    public void verifySearchResultCountMatchesDisplayedItems() {
+        // Arrange
+        String searchItem = "Shoes";
+        WebDriver webDriver = null;
+
+        LauncherPage launcherPage = new LauncherPage(webDriver);
+        launcherPage.navigateTo("https://web-playground.ultralesson.com/");
+
+        // Act
         HomePage homepage = new HomePage(webDriver);
         homepage.search(searchItem);
         List<Item> searchItems = homepage.getSearchItems();
+        int itemCountDisplayed = homepage.getItemCount(); // Assume getItemCount method returns the number displayed on the page
 
-        //Assert
-        Assert.assertEquals(4, searchItems.size());
-        Assert.assertTrue(searchItems.stream().allMatch(item -> item.getName().contains(searchKey)));
+        // Assert
+        Assert.assertEquals(searchItems.size(), itemCountDisplayed);
     }
-
 
 }
